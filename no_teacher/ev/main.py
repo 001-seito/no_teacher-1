b2a6f2f775fd836b1cb3acc90b154fb4991c6c6e
001-seito <77827579+001-seito@users.noreply.github.com>
2021-01-27 16:08:46 +0900

    Update main.py

--- no_teacher/ev/main.py
+++ no_teacher/ev/main.py
@@ -1,100 +1,59 @@
+from ev_command import EV, SPEED, INTERVAL, DIAMETER
 import asyncio
-from time import sleep
-import math
-import matplotlib.pyplot as plt
-import random
 
-TEST_ID = random.randint(0, 1 << 16)
+r = 50
 
-def convert_data(gyro_data, motor_speed, interval, diameter, colors):
-    step = diameter * math.pi * motor_speed * interval / 1000
-    cx = 0.0
-    cy = 0.0
+def first_drive(writer, ev):
+    while not ev.button.up:
+        move_direction, color = ev.steer(r)
+        writer.write("gyro:{}".format(str(move_direction)).encode())
+        if color:
+            writer.write("color:white".encode())
+        else:
+            writer.write("color:black".encode())
 
-    positions = [(0.0, 0.0, True)]
+    ev.tank.off()
+    writer.write("cmd:END_FIRST".encode())
 
-    for (degree, color) in zip(gyro_data, colors):
-        dx = step * math.cos(math.radians(degree))
-        dy = step * math.sin(math.radians(degree))
-        cx += dx
-        cy += dy
-        positions.append((cx, cy, color == "black"))
-        print(cx, cy)
-        plt.plot(cx, cy, '-',)
+def second_drive(ev, reader):
+    while True:
+        message = yield from reader.read(1 << 15)
+        (attr, _, data) = message.decode().partition(':')
+        print(data)
+        if attr == "direction":
+            ev.turn_degrees(float(data))
+        elif attr == "dist":
+            ev.on_for_millis(float(data))
 
-    plt.savefig("course_first_drive@{}.png".format(TEST_ID))
+def client(loop, ev):
+    _, writer = yield from asyncio.open_connection('169.254.225.141', 50010, loop=loop)
+    print("Connected into server")
 
-    return positions
+    data1 = "values:{} {} {}".format(SPEED, INTERVAL, DIAMETER).encode()
+    writer.write(data1)
 
+    print("Starting 1st drive")
+    first_drive(writer, ev)
 
-class Server(asyncio.Protocol):
-    gyro_data = []
-    colors = []
-    motor_speed = 100
-    interval = 100
-    diameter = 55
+    reader, writer = yield from asyncio.open_connection('169.254.225.141', 50010, loop=loop)
+    print("Connected into server")
 
-    def connection_made(self, transport):
-        peername = transport.get_extra_info('peername')
-        print('Connection from {}'.format(peername))
-        self.transport = transport
-
-    def data_received(self, data):
-        message = data.decode()
-        (attr, _, data) = message.partition(':')
-        if attr == "gyro":
-            f = float(data)
-            print(f)
-            self.gyro_data.append(f)
-        elif attr == "color":
-            print(data)
-            self.colors.append(data)
-        elif attr == "cmd" and data == "END_FIRST":
-            print("Close the connect")
-            self.transport.close()
-
-        elif attr == "cmd" and data == "START_SECOND":
-            positions = convert_data(self.gyro_data, self.motor_speed,
-                                     self.interval,  self.diameter, self.colors)
-            print("converted data")
-            black_points = []
-            for (x, y, c) in positions:
-                if c:
-                    black_points.append((x, y))
-
-            current_direction = 0.0
-            for i in range(len(black_points) - 1):
-                (bx, by) = black_points[i]
-                (cx, cy) = black_points[i + 1]
-                next_direction = math.degrees(
-                    math.atan2((cy - by), (cx - bx)))
-                turn_direction = next_direction - current_direction
-                distance = math.sqrt((cx - bx) ** 2 + (cy - by) ** 2)
-                print(turn_direction, distance)
-                self.transport.write(
-                    "direction:{}".format(turn_direction).encode())
-                sleep(0.2)
-                self.transport.write("dist:{}".format(distance).encode())
-                sleep(0.2)
-                current_direction = next_direction
-
+    writer.write("cmd:START_SECOND".encode())
+    print("Starting 2nd drive")
+    second_drive(ev, reader)
 
 def main():
     loop = asyncio.get_event_loop()
-    s = loop.create_server(Server, '169.254.225.141', 50010)  # ip, port
-    server = loop.run_until_complete(s)  # starting server blocking
-    print('Serving on {}'.format(server.sockets[0].getsockname()))
+    ev = EV()
+
+    loop.run_until_complete(client(loop, ev))
 
     try:
-        loop.run_forever()  # loop the tasks until the server is closed or ^C is sent
+        loop.run_forever()
     except KeyboardInterrupt:
-        pass
-
-    server.close()
-    loop.run_until_complete(server.wait_closed())
-    print("Server succesfully closed")
+        loop.close()
+        exit(1)
     loop.close()
-
 
 if __name__ == '__main__':
     main()
